f6accd4cb9b1c5a86ea7aec606c6f847fe9eb3f1
Admin <kristinaalexiuk@yandex.ru>
2026-09-23 21:35:28 +0300

    Написали BuildRating

---
+++ BuildAnalyzer.App/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace BuildAnalyzer.Core;
+
+public enum BuildRating
+{
+    /// <summary>Средний урон менее 1500.</summary>
+    Weak,
+
+    /// <summary>Средний урон от 1500 до 2499.</summary>
+    Normal,
+
+    /// <summary>Средний урон от 2500 до 3499.</summary>
+    Good,
+
+    /// <summary>Средний урон 3500 и более.</summary>
+    Excellent
+}
